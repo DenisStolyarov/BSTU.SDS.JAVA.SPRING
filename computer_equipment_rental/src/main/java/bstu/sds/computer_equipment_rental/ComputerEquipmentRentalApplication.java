e6d32416aa13cba1a6b5eb25060cdd0ee0e2bf70
Denis <denisstolarov27@gmail.com>
2020-10-17 10:17:43 +0300

    Add start project files.

---
+++ computer_equipment_rental/src/main/java/bstu/sds/computer_equipment_rental/ComputerEquipmentRentalApplication.java
@@ -0,0 +1,13 @@
+package bstu.sds.computer_equipment_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerEquipmentRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerEquipmentRentalApplication.class, args);
+	}
+
+}
---
+++ computer_equipment_rental/src/main/java/bstu/sds/computer_equipment_rental/ServletInitializer.java
@@ -0,0 +1,13 @@
+package bstu.sds.computer_equipment_rental;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ComputerEquipmentRentalApplication.class);
+	}
+
+}
